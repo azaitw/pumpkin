e3d8c5578164d20caf7ed0853bbb13259f72c7b0
Bryan Chan <azai@yahoo-inc.com>
2014-10-10 16:29:11 +0800

    Redirect root to beardude

--- api/controllers/portalController.js
+++ api/controllers/portalController.js
@@ -1,8 +1,10 @@
 var portal = {
     index: function (req, res) {
+        return res.redirect('/beardude');
+        /*
         renderService.html(res, 'portal', {
             title: 'Pumpkin Lab, your branding specialist'
-        });
+        });*/
     }
 };
 module.exports = portal;
