bf1e1bb64835bd5652e8514190651a99a5d6da17
Bryan Chan <azai@yahoo-inc.com>
2014-12-08 00:08:47 +0800

    Update isAdmin detection rule

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -45,14 +45,20 @@ var bcrypt = require('bcrypt-nodejs'),
             });
             return deferred.promise;
         },
-        isAdmin: function (input) {
+        isAdmin: function (brandName, input) {
             var q = Q.defer(),
                 output = {
                     isAdmin: false,
                     user: input.email
                 };
-
-            user.findOne({email: input.email})
+            brand.findOne({brandName: brandName})
+            .then(function (D) {
+                if (D.email !== input.email) {
+                    console.log('email doesnt match');
+                    return q.resolve(output);
+                }
+                return user.findOne({email: input.email});
+            })
             .then(function (D) {
                 if (typeof D !== 'undefined') { // user found
                     bcrypt.compare(input.password, D.password, function (err, D1) {
@@ -72,41 +78,46 @@ var bcrypt = require('bcrypt-nodejs'),
         },
         loginPage: function (req, res) {
             var input = req.body,
-                brand = req.params.brand,
+                brandName = req.params.brand,
                 credentials = req.session.credentials;
 
             if (credentials) { // If already authenticated, redirect to manage page
-                return res.redirect('/' + brand + '/manage');
+                return res.redirect('/' + brandName + '/manage');
             }
             if (typeof input === 'undefined') { // request login page
-                return renderService.html(res, 'signup', {
-                    title: '登入 ' + brand,
-                    brand: brand,
-                    js: ['login.js'],
-                    form: [{
-                            type: 'text',
-                            key: 'email',
-                            label: '輸入註冊電子信箱'
-                        },
-                        {
-                            type: 'password',
-                            key: 'password',
-                            label: '輸入密碼'
-                        }
-                    ]
+                brand.findOne({brandName: brandName})
+                .then(function (D) {
+                    return renderService.html(res, 'signup', {
+                        title: '登入 ' + brandName,
+                        brand: D,
+                        js: ['login.js'],
+                        form: [{
+                                type: 'text',
+                                key: 'email',
+                                label: '輸入註冊電子信箱'
+                            },
+                            {
+                                type: 'password',
+                                key: 'password',
+                                label: '輸入密碼'
+                            }
+                        ]
+                    });
                 });
             }
-            userController.isAdmin(input)   // To authenticate
-            .then(function (D) {
-                if (D.isAdmin) {
-                    req.session.credentials = D; // set session credentials
-                    return res.send(true);
-                }
-                return res.send(false);
-            })
-            .catch(function (E) {
-                console.log(E);
-            });
+            if (input) {
+                userController.isAdmin(brandName, input)   // To authenticate
+                .then(function (D) {
+                    if (D.isAdmin) {
+                        req.session.credentials = D; // set session credentials
+                        return res.send(true);
+                    }
+                    return res.send(false);
+                })
+                .catch(function (E) {
+                    console.log(E);
+                });
+            }
         }
     };
 
