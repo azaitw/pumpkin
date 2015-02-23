23d46351465e324c0515dd7a197e47a0af73bc4d
Azai <mightybj@gmail.com>
2015-02-23 19:17:26 +0800

    Remove dev model config to temporarily fix heroku problem

--- config/env/development.js
+++ config/env/development.js
@@ -16,9 +16,7 @@ module.exports = {
    * Set the default database connection for models in the development       *
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
-    models: {
-            connection: 'localMongo'
-        },
+
     log: {
         level: "verbose"
     }
