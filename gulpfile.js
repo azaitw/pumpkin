f33e3fa8a61e2a201409c23237e3b305e3a450b2
Azai <mightybj@gmail.com>
2015-03-08 22:03:29 +0800

    Istanbul coverage task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 var shell = require('gulp-shell');
 var jshint = require('gulp-jshint');
 var testCommands = ['cd <%=file.path %>;npm install ../..;npm prune;cp -R ../../node_modules node_modules;npm install;npm run-script disc;npm test'];
@@ -11,13 +12,25 @@ gulp.task('unit_test', function () {
         .pipe(mocha({reporter: 'nyan'}));
 });
 
-gulp.task('smoke_test', function () {
-    return gulp.src('examples/00hello/')
-    .pipe(shell(testCommands));
+gulp.task('coverage', function () {
+    return gulp.src(['api/**/*.js', '*.js'])
+        .pipe(istanbul({includeUntested: true}))
+        .pipe(istanbul.hookRequire())
+        .on('finish', function () {
+            gulp.src(['test/**/*.spec.js'])
+            .pipe(mocha())
+            .pipe(istanbul.writeReports());
+        });
 });
 
-gulp.task('example_tests', function () {
-    return gulp.src('examples/*-*/')
+gulp.task('lint', function() {
+  return gulp.src(['api/**/*.js', 'config/**/*.js', 'test/**/*.js', '*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('smoke_test', function () {
+    return gulp.src('examples/00hello/')
     .pipe(shell(testCommands));
 });
 
@@ -26,9 +39,3 @@ gulp.task('watch_document', ['build_document'], function () {
 });
 
 gulp.task('build_document', shell.task('jsdoc -p README.md index.js lib/*.js extra/*.js -d documents'));
-
-gulp.task('lint', function() {
-  return gulp.src(['api/**/*.js', 'config/**/*.js', 'test/**/*.js', '*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
