512d6a142017de0611252259d0dec33113214a37
azai-yahoo <azai@yahoo-inc.com>
2015-07-06 23:37:30 +0800

    Udpate render service

--- api/services/renderService.js
+++ api/services/renderService.js
@@ -4,29 +4,6 @@ module.exports = {
     _config: {
 
     },
-    returnTimeObj: function () {
-        if (typeof dateTimeService !== 'undefined') {
-            return {
-                year: dateTimeService.getYear(),
-                month: dateTimeService.getMonth(),
-                day: dateTimeService.getDay()
-            };
-        }
-        return {};
-    },
-    returnBrandObj: function (brandName) {
-        var q = Q.defer();
-        brand.findOne({slug: brandName})
-        .then(function (D) {
-            delete D.createdAt;
-            delete D.updatedAt;
-            return q.resolve(D);
-        })
-        .catch(function (E) {
-            return q.resolve(false);
-        });
-        return q.promise;
-    },
     htmlOld: function (req, res, params) {
         var i;
         var renderObj = {};
@@ -90,6 +67,7 @@ module.exports = {
         title: 'TITLE'
     }
     */
+    // To be deprecated
     renderHtml: function (res, params) {
         var i;
         var renderObj = {};
@@ -112,54 +90,6 @@ module.exports = {
         renderObj.time = this.returnTimeObj();
         return res.render('layout', renderObj);
     },
-    //params for data, options for templates
-    // auto-load page-specific js
-    html: function (req, res, params, options) {
-        var renderObj = params;
-        var brandName;
-        var actionName = req.options.action;
-        var templates = options.templates || {};
-        var jsFile;
-        var q = Q.defer();
-        // brand name
-        if (req.params && req.params.brand) {
-            brandName = req.params.brand;
-        }
-        // js file
-        if (actionName.indexOf('page') > 0) {
-            jsFile = actionName.substring(0, actionName.indexOf('page'));
-            renderObj.js = jsFile + '.js';
-        }
-        // template
-        renderObj.partials = {
-            css: 'lib/css',
-            script: 'lib/script',
-            header: templates.header || 'header',
-            footer: templates.footer || 'footer',
-            sourceDecoration: 'lib/sourceDecoration',
-            body: templates.body || 'lib/form'
-        };
-        if (renderObj.partials.body === 'lib/form') {
-            renderObj.formAction = req.route.path;
-        }
-        // time
-        renderObj.time = this.returnTimeObj();
-
-        if (typeof brandName !== 'undefined') {
-            // render with brand obj
-            this.returnBrandObj(req.params.brand)
-            .then(function (D) {
-                if (D) {
-                    renderObj.brand = D;
-                }
-                return q.resolve(res.render('layout', renderObj));
-            });
-        } else {
-            // render without brand obj
-            return q.resolve(res.render('layout', renderObj));
-        }
-        return q.promise;
-    },
     email: function (res, templateName, params) {
         var i,
             renderObj = {};
@@ -170,5 +100,105 @@ module.exports = {
         }
         return res.render(templateName, renderObj);
 
+    },
+/*
+======================
+2.0 render services
+======================    
+*/
+    returnTimeObj: function () {
+        if (typeof dateTimeService !== 'undefined') {
+            return {
+                year: dateTimeService.getYear(),
+                month: dateTimeService.getMonth(),
+                day: dateTimeService.getDay()
+            };
+        }
+        return {};
+    },
+    returnBrandObj: function (brandName) {
+        var q = Q.defer();
+        brand.findOne({slug: brandName})
+        .then(function (D) {
+            delete D.createdAt;
+            delete D.updatedAt;
+            return q.resolve(D);
+        })
+        .catch(function (E) {
+            return q.resolve(false);
+        });
+        return q.promise;
+    },
+    /* translate abcPage action to abc, and append /js/page/abc.js and /styles/page/abc.js links
+    returnStaticFiles: function (actionName) {
+        var jsFiles = [];
+        var cssFiles = [];
+        var fileName;
+        if (actionName.indexOf('page') > 0) {
+            fileName = actionName.substring(0, actionName.indexOf('page'));
+            jsFiles.push('/js/page/' + fileName + '.js');
+            cssFiles.push('/styles/page/' + fileName + '.css');
+        }
+        return [jsFiles, cssFiles];
+    },
+    */
+    returnTemplates: function (templates) {
+        return {
+            css: 'lib/css',
+            script: 'lib/script',
+            header: templates.header || 'header',
+            footer: templates.footer || 'footer',
+            sourceDecoration: 'lib/sourceDecoration',
+            body: templates.body || 'lib/form',
+            form: 'lib/form'
+        };
+    },
+    appendModuleFiles: function (renderObj, modules) {
+        var i;
+        var placeholder;
+        if (typeof modules === 'undefined') {
+            return renderObj;
+        }
+        for (i = 0; i < modules.length; i += 1) {
+            placeholder = modules[i];
+            renderObj.js.push('/js/modules/' + placeholder + '.js');
+            renderObj.css.push('/styles/modules/' + placeholder + '.css');
+            renderObj.partials[placeholder] = 'modules/' + placeholder;
+        }
+        return renderObj;
+    },
+    //contentObj: {}
+    //options: (optional) formAction, templates and js
+    /*
+    options: (optional) {
+        templates: {}
+        js: [],
+        css: [],
+        module: []
+    }
+    */
+    html: function (req, res, contentObj, options) {
+        var opts = options || {};
+        var q = Q.defer();
+        var renderObj = JSON.parse(JSON.stringify(contentObj));
+        renderObj.js = opts.js || []; // JS files
+        renderObj.css = opts.css || []; // CSS files
+        renderObj.partials = this.returnTemplates(opts.templates); // templates
+        renderObj.time = this.returnTimeObj(); // time
+        this.appendModuleFiles(renderObj, opts.modules); // modules
+
+        if (req.params && req.params.brand) { // add brand info to contentObj if available
+            this.returnBrandObj(req.params.brand)
+            .then(function (D) {
+                renderObj.brand = D;
+                contentObj.brand = D;
+                renderObj.paramsRaw = JSON.stringify(contentObj); // For client-side rendering
+                return q.resolve(res.render('layout', renderObj));
+            });
+        } else { // render without brand obj
+            renderObj.paramsRaw = JSON.stringify(contentObj); // For client-side rendering
+            return q.resolve(res.render('layout', renderObj));
+        }
+        return q.promise;
     }
 };
